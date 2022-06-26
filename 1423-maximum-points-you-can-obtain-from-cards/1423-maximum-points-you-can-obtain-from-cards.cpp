2b772f4361b82b05e1f6ed9a29d59bc08e59931a
Aditi khatri <63184114+aditikhatri@users.noreply.github.com>
2022-06-27 00:25:15 +0530

    Time: 83 ms (57.55%), Space: 42.5 MB (48.14%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     int maxScore(vector<int>& card, int k) {
-         int res = 0;
-        for(int i=0;i<k;i++) res+=card[i];
-        
-        int curr=res;
-        for(int i=k-1;i>=0;i--) {
-            curr-=card[i];
-            curr+=card[card.size()-k+i];
-			res = max(res, curr);
+      int n=card.size();
+        int msum,sum,temp;
+        for(int i=0;i<n-k;i++){
+            msum+=card[i];
         }
+        sum=temp=msum;
+        for(int i=n-k;i<n;i++){
+            sum+=card[i];
+            msum=min(msum,(temp-card[i-n+k]+card[i]));
+            temp=(temp-card[i-n+k]+card[i]);
+        }
+        return sum-msum;
+//          int res = 0;
+//         for(int i=0;i<k;i++) res+=card[i];
+//         int curr=res;
+//         for(int i=k-1;i>=0;i--) {
+//             curr-=card[i];
+//             curr+=card[card.size()-k+i];
+// 			res = max(res, curr);
+//         }
         
-        return res;
+//         return res;
     }
 };
-// we need to check max window made by of first/last k elements
+// mtd 1->can be converted to contig array sum 
+//-sliding window->we need to check max window made by of first/last k elements
