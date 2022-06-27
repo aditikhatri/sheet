9dd5492ac52b1410c14c84c1a0049224f06e8575
Aditi khatri <63184114+aditikhatri@users.noreply.github.com>
2022-06-27 11:29:37 +0530

    Time: 38 ms (66.18%), Space: 13.4 MB (92.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minPartitions(string n) {
+        //return *max_element(n.begin(),n.end())-'0';
         int maxi=0;
         for(auto i:n){
             maxi=max(maxi,i-'0');
@@ -8,3 +9,5 @@ public:
         return maxi;
     }
 };
+//by observation 32 -11= 22-11= 11 -11 =0 
+//so we need atleast 3 d-b no i.e max digit in string 
