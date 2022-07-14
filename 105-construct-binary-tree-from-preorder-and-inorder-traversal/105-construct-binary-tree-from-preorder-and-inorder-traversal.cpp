e13a6d804a201a681fe3a255b50cb084472e3dc8
Aditi khatri <63184114+aditikhatri@users.noreply.github.com>
2022-07-15 01:16:44 +0530

    Time: 17 ms (90.89%), Space: 26.4 MB (47.35%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,36 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
- class Solution {
+class Solution {
 public:
-       int preIndex=0;
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int n = inorder.size();
-        return help(preorder,inorder,0, n-1);
-    }
-    
-    TreeNode* help(vector<int>& preorder, vector<int>& inorder,int left,int right)
-    {
-        
-      
-        
-        if(left>right)
-            return NULL;
-        TreeNode* root = new TreeNode(preorder[preIndex++]);
-        int inIndex=0;
-        for(int j=left;j<=right;j++)
-        {
-            if(inorder[j]==root->val)
-            {
-                inIndex=j;
-                break;
-            }
+        map<int ,int>mp;
+        int n=inorder.size();
+        for(int i=0;i<n;i++){
+            mp[inorder[i]]=i; //search inorder
         }
-        root->left = help(preorder,inorder,left,inIndex-1);
-        root->right = help(preorder,inorder,inIndex+1,right);
-        return root;
-    
+        TreeNode* root= helper(preorder,0,n-1,inorder,0,n-1,mp);
         
+        return root;
+    }
+    TreeNode*helper(vector<int>& preorder, int ps,int pe,vector<int>& inorder,int is,int ie,map<int,int>&mp){
+        if(is>ie || ps>pe) return NULL;
+        // declare root
+        TreeNode*root= new TreeNode(preorder[ps]);
+        int idx=mp[root->val]; //inorder ke liye
+        int diff= idx-is;
+        root->left= helper(preorder,ps+1,ps+diff,inorder,is,idx-1,mp);
+        root->right=helper(preorder,ps+diff+1,pe,inorder,idx+1,ie,mp);
+        return root;
     }
 };
