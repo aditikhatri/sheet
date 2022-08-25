91570635cb57d1cb5a9485d7bcdb25ab50eb47f2
Aditi khatri <63184114+aditikhatri@users.noreply.github.com>
2022-08-26 00:35:49 +0530

    Time: 33 ms (37.61%), Space: 8.9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -5,14 +5,10 @@ public:
         
         unordered_map<char,int>mp;
         for(int i=0;i<magazine.size();i++){
-            // store frequrncy of character of magazine
             mp[magazine[i]]++;
         }
         for(int i=0;i<ransomNote.size();i++){
-            // If character from ransomNote is not present in map then return false
             if(mp[ransomNote[i]]==0)return false;
-            
-            //If character is present then decrease its value by 1
             mp[ransomNote[i]]--;
         }
         return true;
